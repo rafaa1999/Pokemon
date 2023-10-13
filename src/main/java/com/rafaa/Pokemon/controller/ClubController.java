fcb63115acdd33f05cc547b728dd62703d83c38d
rafaa1999 <raferhouma10@gmail.com>
2023-10-13 21:50:40 +0100

    Added Validation

--- src/main/java/com/rafaa/Pokemon/controller/ClubController.java
+++ src/main/java/com/rafaa/Pokemon/controller/ClubController.java
@@ -4,9 +4,12 @@ import com.rafaa.Pokemon.dto.ClubDto;
 import com.rafaa.Pokemon.model.Club;
 import com.rafaa.Pokemon.service.ClubService;
 import com.rafaa.Pokemon.service.Impl.ClubServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,8 +37,14 @@ public class ClubController {
         return "clubs-create";
     }
     @PostMapping("/clubs/new")
-    public String saveClub(@ModelAttribute("club") Club club ){
-       clubService.saveClub(club);
+    public String saveClub(@Valid @ModelAttribute("club") ClubDto clubDto,
+                           BindingResult result,
+                           Model model){
+       if(result.hasErrors()){
+           model.addAttribute("club", clubDto);
+           return "clubs-create";
+       }
+       clubService.saveClub(clubDto);
        return "redirect:/clubs";
     }
     @GetMapping("/clubs/{clubId}/edit")
@@ -45,7 +54,12 @@ public class ClubController {
         return "clubs-edit";
     }
     @PostMapping("/clubs/{clubId}/edit")
-    public String updateClub(@PathVariable("clubId") long clubId, @ModelAttribute("club") ClubDto club ){
+    public String updateClub(@PathVariable("clubId") long clubId,
+                             @Valid @ModelAttribute("club") ClubDto club,
+                             BindingResult result){
+        if(result.hasErrors()){
+            return "clubs-edit";
+        }
         club.setId(clubId);
         clubService.updateClub(club);
         return "redirect:/clubs";
